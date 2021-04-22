65dc69c111c66816ad6d9c0595706adf5276ae25
Atilla <krbck.atilla@gmail.com>
2021-04-22 17:14:31 +0300

    Created website files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$('#topheader .navbar-nav a').on('click', function () {
+  $('#topheader .navbar-nav').find('li.active').removeClass('active');
+  $(this).parent('li').addClass('active');
+});
+
+// AOS ANIMATION
+AOS.init({
+  disable: 'mobile',
+  duration: 800,
+  anchorPlacement: 'center-bottom',
+});
+
+// SMOOTH SCROLL
+$(function () {
+  $('.nav-link').on('click', function (event) {
+    var $anchor = $(this);
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr('href')).offset().top - 0,
+        },
+        1000
+      );
+    event.preventDefault();
+  });
+});
